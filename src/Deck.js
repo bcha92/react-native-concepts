4a9e5f43b3ca946ab0d5db4c9f8fdc1d1fe5edb6
Brandon Cha <brandon.cha@redspace.com>
2022-04-05 14:57:47 -0400

    --gestures-pan-responders

--- src/Deck.js
+++ src/Deck.js
@@ -1,13 +1,27 @@
-import React from "react";
-import { View, Animated } from "react-native";
+import React, { useState } from "react";
+import { View, Animated, PanResponder } from "react-native";
 
-// Deck Component (Animated)
+// Deck Component
 const Deck = ({ data, renderCard }) => {
+    const position = new Animated.ValueXY();
+    // User Input, Animation Output panResponder state based on position
+    const [ panResponder ] = useState(PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (event, gesture) => {
+            position.setValue({ x: gesture.dx, y: gesture.dy })
+        },
+        onPanResponderRelease: () => {}
+    }));
+
     const renderCards = () => data.map(item => renderCard(item));
+
     return (
-        <View>
+        <Animated.View
+            style={position.getLayout()}
+            {...panResponder.panHandlers}
+        >
             {renderCards()}
-        </View>
+        </Animated.View>
     );
 };
 
